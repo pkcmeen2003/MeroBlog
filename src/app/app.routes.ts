79be5d1a8691c5062a224bf8f31dcf9b3d3fd1fb
Meen Khatri <meen_khatri@yahoo.com>
2024-10-02 00:41:50 -0400

    added multiple fuctions

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AddPostComponent } from './pages/add-post/add-post.component';
 
 export const routes: Routes = [
 
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'home', component: HomePageComponent },  // HomePage route
+  { path: 'add-post', component: AddPostComponent},
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/login' }
     
